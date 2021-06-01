6b2be7d49de109b447c051fa0156868e26a2a95f
iwasakishuto <cabernet.rock@gmail.com>
2021-06-02 00:23:23 +0900

    :walking: Minor Updates.

--- teilab/utils/__init__.py
+++ teilab/utils/__init__.py
@@ -1,15 +1,19 @@
 #coding: utf-8
 from ._path import *
 from ._config import *
+from . import dataset_utils
 from . import download_utils
 from . import generic_utils
+
+
+from .dataset_utils import get_teilab_data
+from .dataset_utils import get_filelists
 
 from .download_utils import unzip
 from .download_utils import decide_extension
 from .download_utils import Downloader
 from .download_utils import GoogleDriveDownloader
 from .download_utils import decide_downloader
-from .download_utils import get_teilab_data
 
 from .generic_utils import now_str
 from .generic_utils import readable_bytes
--- teilab/utils/_path.py
+++ teilab/utils/_path.py
@@ -2,10 +2,11 @@
 import os
 
 __all__ = [
-    "UTILS_DIR", "MODULE_DIR", "REPO_DIR", "DATA_DIR",
+    "UTILS_DIR", "MODULE_DIR", "REPO_DIR", "DATA_DIR", "SAMPLE_LIST_PATH",
 ]
 
-UTILS_DIR     = os.path.dirname(os.path.abspath(__file__)) #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments/teilab/utils
-MODULE_DIR    = os.path.dirname(UTILS_DIR)                 #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments/teilab
-REPO_DIR      = os.path.dirname(MODULE_DIR)                #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments
-DATA_DIR      = os.path.join(REPO_DIR, "data")             #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments/data
+UTILS_DIR        = os.path.dirname(os.path.abspath(__file__)) #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments/teilab/utils
+MODULE_DIR       = os.path.dirname(UTILS_DIR)                 #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments/teilab
+REPO_DIR         = os.path.dirname(MODULE_DIR)                #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments
+DATA_DIR         = os.path.join(REPO_DIR, "data")             #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments/data
+SAMPLE_LIST_PATH = os.path.join(DATA_DIR, "sample_list.txt")  #: path/to/TeiLab-BasicLaboratoryWork-in-LifeScienceExperiments/data/sample_list.txt
